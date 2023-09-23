fc19183c84965394cfeede28cdcbe488cda4b1e3
Harry Vince <harryavince@gmail.com>
2023-09-23 09:47:20 +0100

    dynamic title pages + fix for prettier

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import title from '$lib/stores/title';
+
+	$: pageTitle = $title;
+</script>
+
+<svelte:head>
+	<title>{pageTitle ? `${pageTitle} - Todos` : 'Todos'}</title>
+</svelte:head>
+
+<body>
+	<main class="container">
+		<slot />
+	</main>
+</body>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script lang="ts">
+	import title from '$lib/stores/title';
+	title.set('Login');
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
